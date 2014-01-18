8744b821a35d6f1a38694e29771d7031910c2499
Martin Lowinski <martin@goldtopf.org>
2014-01-18 21:00:16 +0100

    Add comments.

--- config.example.php
+++ config.example.php
@@ -1,15 +1,24 @@
 <?php
 
+// Path to logs (write access needed)
 define('LOG', 'logs/dyndns.log');
+
+// Credentials and configuration provided by SchlundTech
 define('HOST', 'https://gateway.schlundtech.de');
 define('USER', '');
 define('PASSWORD', '');
 define('CONTEXT', 42);
+
+// Templates to access the gateway
 define('XML_GET_ZONE', 'request-get.xml');
 define('XML_PUT_ZONE', 'request-put.xml');
+
+// Domain configuration
 define('DOMAIN', 'example.com');
 define('SUBDOMAIN', 'home');
 define('SYSTEM_NS', 'ns.example.com');
+
+// Credentials to access this service
 define('REMOTE_PASS', 'mylongdyndnspassword');
 
 ?>
